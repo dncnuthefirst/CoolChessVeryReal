b31e0a376efc96c428d54ac52c3443bcea51e486
dncnuthafirst <148245840+dncnuthefirst@users.noreply.github.com>
2024-07-13 21:49:03 +0800

    plz dont hurt me

---
+++ main.js
@@ -0,0 +1,37 @@
+var abChess = {};
+var config = {
+    clickable: false,
+    draggable: false
+};
+var gamePGN = document.getElementById("game-code");
+var promotions = ["b", "n", "q", "r"];
+abChess = new AbChess("chessboard", config);
+abChess.setFEN();
+
+function chooseRandom(array) {
+    var random = Math.floor(Math.random() * array.length);
+    return array[random];
+}
+
+function playRandomMove(index) {
+    var legalMoves = abChess.getLegalMoves(index);
+    var move = chooseRandom(legalMoves);
+    var promotion = chooseRandom(promotions);
+    abChess.play(move.start, move.end, promotion);
+    gamePGN.innerText = abChess.getPGN();
+    index += 1;
+    if (abChess.is50Moves(index) ||
+        abChess.isCheckmate(index) ||
+        abChess.isInsufficientMaterial(index) ||
+        abChess.isStalemate(index)) {
+        document.getElementById("Hmm").textContent = "GGs. Refresh to try again i guess"
+        return;
+    }
+    timeout = setTimeout(function () {
+        playRandomMove(index);
+    }, 400);
+}
+
+setTimeout(function () {
+    playRandomMove(0);
+}, 3000);
